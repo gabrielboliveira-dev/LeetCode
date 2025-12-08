f65f137a4e428c06805a22b32182c58832be100d
Gabriel Oliveira <gabrielgbo00@gmail.com>
2025-12-08 09:00:59 -0300

    feat(java): add news 4 LeetCodes

---
+++ Java/LowestCommonAncestorOfABST.java
@@ -0,0 +1,18 @@
+package Java;
+
+public class LowestCommonAncestorOfABST {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode current = root;
+
+        while (current != null) {
+            if (p.val < current.val && q.val < current.val) {
+                current = current.left;
+            } else if (p.val > current.val && q.val > current.val) {
+                current = current.right;
+            } else {
+                return current;
+            }
+        }
+        return null;
+    }
+}
