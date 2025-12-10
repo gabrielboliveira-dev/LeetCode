dd5b3e1a695c2e03ed86e790292df2e5554e4dcd
Gabriel Oliveira <gabrielgbo00@gmail.com>
2025-12-10 10:58:05 -0300

    feat(java): add new 1 LeetCode

---
+++ Java/ValidateBinarySearchTree.java
@@ -0,0 +1,24 @@
+package Java;
+
+public class ValidateBinarySearchTree {
+    public boolean isValidBST(TreeNode root) {
+
+        return validate(root, null, null);
+    }
+
+    private boolean validate(TreeNode node, Integer min, Integer max) {
+        if (node == null) {
+            return true;
+        }
+
+        if (min != null && node.val <= min) {
+            return false;
+        }
+
+        if (max != null && node.val >= max) {
+            return false;
+        }
+
+        return validate(node.left, min, node.val) && validate(node.right, node.val, max);
+    }
+}
